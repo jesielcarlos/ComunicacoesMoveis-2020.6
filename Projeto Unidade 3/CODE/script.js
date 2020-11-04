97371966e73c98ae04990177a327fb3f004e8b4b
jesielcarlos <jesielcarlos17@gmail.com>
2020-11-04 14:30:58 -0300

    Bootstrap

--- Projeto Unidade 3/CODE/script.js
+++ Projeto Unidade 3/CODE/script.js
@@ -1,4 +1,9 @@
 function calcular(){
-    window.alert('Testando....')
+    //window.alert('Testando....')
+    var bw = $('#BW').val()
+    var cp = $('#CP').val()
 
+    var result = parseFloat(bw) + parseFloat(cp)
+
+    $('#prb').html(result);
 }
