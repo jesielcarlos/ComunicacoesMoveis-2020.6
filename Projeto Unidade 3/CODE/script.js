07d274d361959b1804b5a7721c29b09ccb873b22
jesielcarlos <jesielcarlos17@gmail.com>
2020-11-04 20:41:01 -0300

    Aprimoramento no estilo das saidas

--- Projeto Unidade 3/CODE/script.js
+++ Projeto Unidade 3/CODE/script.js
@@ -1,5 +1,13 @@
 function calcular(){
-    window.alert('Testando....')
+    //window.alert('Testando....')
+    var bw   = window.document.getElementById('BW').value
+    var cp   = window.document.getElementById('CP').value
+    var msc  = window.document.getElementById('BW').value
+    var mimo = window.document.getElementById('BW').value
+    var ca   = window.document.getElementById('BW').value
+    var prb =  window.document.getElementById('prb')
+    var result = parseFloat(bw) + parseFloat(cp)
 
-
+    prb.innerHTML= result
+    
 }
